1a34cdb6fbfc9106666414a9a433ec09fdb86b41
Mohamed Duse <duseibnuduse1212@gmail.com>
2024-03-20 17:37:41 +0300

    added some updates
    added couple of changes

--- lib/postClass.dart
+++ lib/postClass.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:io';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -13,35 +14,37 @@ class userpost {
   String post;
   int reactions;
   Image profile;
+  Image postImage;
 
   userpost(
       {required this.id,
       required this.name,
       required this.post,
       required this.reactions,
-      required this.profile});
+      required this.profile,
+      required this.postImage});
 }
 
-// class Sawiro {
-//   var image1 = 'assets/images/free-images.jpg';
-//   var image2 = 'assets/images/image2.jpg';
-//   var image3 = 'assets/images/images3.jpg';
-//   var image4 = 'assets/images/images4.jpg';
-
-//   Sawiro(
-//     this.image1,
-//     this.image2,
-//     this.image3,
-//     this.image4,
-//   );
-// }
+class Sawiro {
+  var image1 = 'assets/images/eyes.webp';
+  var image5 = 'assets/images/duse.jpeg';
+  var image6 = 'assets/images/image6.jpg';
+}
 
 final List<userpost> userinfo = List.generate(
     10,
     (index) => userpost(
-        id: index + 1,
-        name: faker.person.name(),
-        post: faker.lorem.sentences(5).toString(),
-        reactions: Random().nextInt(200),
-        profile: Image.network(
-            'https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547_960_720.jpg')));
+          id: index + 1,
+          name: faker.person.name(),
+          post: faker.lorem.sentences(5).toString(),
+          reactions: Random().nextInt(200),
+          //postImage: Image.asset(Sawiro().image6),
+          postImage: Image.asset('${picture.values.last}'),
+          profile: Image.asset('${picture.values}'),
+        ));
+// profile: Image.asset('assets/images/${picture.keys.indexed}')));
+
+var picture = <String, String>{
+  "image1": "assets/images/image6.jpg",
+  "image2": "assets/images/duse.jpeg"
+};
